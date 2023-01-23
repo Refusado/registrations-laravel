45fad04e7cb37c54862f78f2ada5b3eeec5b36a3
Renan <orefusado@gmail.com>
2023-01-22 22:14:24 -0300

    feat: validando formulário e alertando os erros

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,12 @@ class UserController extends Controller
 
     public function save(Request $request)
     {
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email",
+            "password" => "required|min:8"
+        ]);
+
         dd($request->all());
     }
 }
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -9,16 +9,34 @@
         <div class="field">
             <label for="name">Name</label>
             <input type="text" name="name" id="name">
+
+            @if($errors->has('name'))
+                @foreach($errors->get('name') as $error)
+                    <strong class="error">{{ $error }}</strong>
+                @endforeach
+            @endif
         </div>
 
         <div class="field">
             <label for="email">Email</label>
             <input type="email" name="email" id="email">
+
+            @if($errors->has('email'))
+                @foreach($errors->get('email') as $error)
+                    <strong class="error">{{ $error }}</strong>
+                @endforeach
+            @endif
         </div>
 
         <div class="field">
             <label for="password">Password</label>
             <input type="password" name="password" id="password">
+
+            @if($errors->has('password'))
+                @foreach($errors->get('password') as $error)
+                    <strong class="error">{{ $error }}</strong>
+                @endforeach
+            @endif
         </div>
 
         <div class="btn">
